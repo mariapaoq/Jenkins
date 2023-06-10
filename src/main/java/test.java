76815add9ce08da22ad615439b7c96b1514a9587
Paola <maria_quintero82172@elpoli.edu.co>
2023-06-10 12:23:16 -0500

    error test1

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -2,5 +2,8 @@ public class test {
     public static void main(String[] args) {
         String password = "password";
         System.out.println(password);
+        String username = "admin";
+        String sql = "SELECT * FROM users WHERE username='" + username + "' AND password='" + password + "'";
+
     }
 }
